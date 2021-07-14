f6f5a751eb7d100ccd280b2a801c51120b2fac2d
Daniel Washington <danielvertigo@hotmail.com>
2021-07-13 21:24:52 -0300

    Files

--- perfilProfile/ViewController.swift
+++ perfilProfile/ViewController.swift
@@ -8,12 +8,38 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var myTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+    
     }
 
-
+    @IBAction func aboutMeButton(_ sender: UIButton) {
+        self.performSegue(withIdentifier: "aboutMe", sender: nil)
+        
+    }
+    @IBAction func websiteButton(_ sender: UIButton) {
+        self.performSegue(withIdentifier: "website", sender: nil)
+        
+    }
+    
+    @IBAction func petsButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func plantsButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func codeButton(_ sender: UIButton) {
+    }
+    @IBAction func appButton(_ sender: UIButton) {
+    }
+    
+    
 }
+
 
